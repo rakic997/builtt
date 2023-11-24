9678aab1b57337c7a5d49a3ff97d08b513d36711
David <mianikic11119@gmail.com>
2023-11-24 18:29:14 +0100

    general structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login';
+import Products from './pages/Login';
+import Cart from './pages/Login';
+import NotFound from './pages/404';
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <div className='content'>
+      <Routes>
+        <Route path="/product/:id" element={<Login />} />
+        <Route path="/" element={<Products />} />
+        <Route path="/products/:id" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   )
 }
 
